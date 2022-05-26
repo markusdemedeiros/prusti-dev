d24cde74496ec994a371727838b46de508b837e5
Vytautas Astrauskas <vastrauskas@gmail.com>
2022-05-26 12:06:56 +0200

    Ugly commit

--- prusti-interface/src/environment/mir_body/mod.rs
+++ prusti-interface/src/environment/mir_body/mod.rs
@@ -4,6 +4,7 @@ use rustc_middle::mir;
 pub mod borrowck;
 pub mod graphviz;
 pub mod patch;
+pub mod patch_fixer;
 
 pub fn apply_patch<'tcx>(
     patch: self::patch::MirPatch<'tcx>,
@@ -11,6 +12,7 @@ pub fn apply_patch<'tcx>(
     borrowck_input_facts: &mut AllInputFacts,
     location_table: &mut LocationTable,
 ) -> mir::Body<'tcx> {
+    let patch = self::patch_fixer::fix_patch(body, patch);
     let mut patched_body = body.clone();
     patch.clone().apply(&mut patched_body);
     apply_patch_to_borrowck(borrowck_input_facts, location_table, &patch, &body, &patched_body);
